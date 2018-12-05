9bb307eb96c0ebbf30dffa77f71114a80aec05db
shreyanikkam11 <shreyanikkam11@gmail.com>
2018-12-05 01:26:09 -0500

    Add files via upload

---
+++ source2.py
@@ -0,0 +1,23 @@
+import csv
+import sys
+csv.field_size_limit(sys.maxsize)
+try:
+    risk = open('Consumer_Complaints.csv', 'r').read() #find the file
+
+except:
+    while risk != "Consumer_Complaints.csv":  # if the file cant be found if there is an error
+     print("Could not open", risk, "file")
+     risk = input("\nPlease try to open file again: ")
+else:
+    with open("Consumer_Complaints.csv") as f:
+        reader = csv.reader(f, delimiter=' ', quotechar='|')
+
+        data = []
+        for row in reader:
+             #print(', '.join(row)
+            print row[5]
+            for col in (1,5):
+                data.append(row)
+                data.append(col)
+        for item in data:
+            print(item,30) #print the rows and columns
